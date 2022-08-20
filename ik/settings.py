65b5fba1c97707437ee7f253daa15afe44cb2ff4
ahmetkirmizioglu <ahmetkirmizioglu@gmail.com>
2022-08-20 17:52:28 +0300

    Debug: False

--- ik/settings.py
+++ ik/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^&z&lq8vn!1hw7zq0mez6z)(8^7u-a#y#*(kw3sjil_k936088'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS=['*']
 
